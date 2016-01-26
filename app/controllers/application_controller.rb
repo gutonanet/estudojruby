8b9635725e920f9b2ed9a6ef5cc4e72efb09fac9
Augusto Lemes <gutonanet@hotmail.com>
2016-01-25 23:00:50 -0200

    Atualizar projeto

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessaoHelper
+  
+  def authorize 
+    unless logged_in? 
+      redirect_to root_url 
+    end 
+  end
+  
+  def correct_user? 
+    @jogador = Jogador.find(params[:id]) 
+      unless current_jogador == @jogador 
+        redirect_to jogador_path 
+      end 
+  end
+
+
 end
--- app/controllers/jogador_controller.rb
+++ app/controllers/jogador_controller.rb
@@ -1,5 +1,12 @@
 class JogadorController < ApplicationController
-  
+   before_action :authorize, except: [:new, :create]
+   before_action :correct_user?, only: [:edit, :update, :destroy]
+   def index 
+     @jogadores = Jogador.all 
+   end 
+   def show 
+     @jogador = Jogador.find(params[:id]) 
+   end
    def new 
      @jogador = Jogador.new 
    end 
@@ -17,12 +24,29 @@ class JogadorController < ApplicationController
      end 
    end 
    
+   def edit 
+      @jogador = Jogador.find(params[:id])  
+   end 
+   def update 
+      @jogador = Jogador.find(params[:id]) 
+      if @jogador.update_attributes(user_params) 
+        redirect_to users_path 
+      else 
+        render action: :edit 
+      end 
+   end 
+  def destroy 
+    @jogador = Jogador.find(params[:id]) 
+    @jogador.destroy 
+    sign_out 
+    redirect_to root_path 
+  end
+
+   
+   
    private 
    def jogador_params 
       params.require(:jogador).permit(:login, :email, :password, :password_confirmation) 
    end
 
-
-
-  
 end
